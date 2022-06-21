103479ad7b2ed184276d44c10efd3eeb8d5a8c6d
Dylan Barber <dylan@dlbarber.dev>
2022-06-22 03:13:28 +0800

    Created LEDMatrix header and library entry point

---
+++ src/LEDMatrix.h
@@ -0,0 +1,27 @@
+#include "Point.h"
+
+#define MATRIX_LENGTH 32
+
+struct LEDMatrix_PinSetup {
+    char latch;  // LAT
+    char clock;  // CLK
+    char data;   // DI
+    char enable; // G
+    char a;      // A
+    char b;      // B
+    char c;      // C
+    char d;      // D
+};
+
+class LEDMatrix {
+    private:
+        LEDMatrix_PinSetup pins;
+        char *buffer;
+        char activeBuffer;
+    public:
+        LEDMatrix(LEDMatrix_PinSetup pinSetup);
+        void setPixel(Point p, bool state);
+        bool getPixel(Point p, bool state);
+        bool getShownPixel(Point p);
+        void finishFrame();
+};
---
+++ src/duinotech-xc4607.h
@@ -0,0 +1,1 @@
+#include "LEDMatrix.h";
