fd70cbff42d7284cd259537b08db4c7232267248
Dylan Barber <dylan@dlbarber.dev>
2022-06-23 15:37:04 +0800

    Created BufferedFrame header

---
+++ src/BufferedFrame.h
@@ -0,0 +1,32 @@
+#ifndef BufferedFrame_h
+#define BufferedFrame_h
+
+#include "LEDMatrix.h"
+#include "Point.h"
+
+#define LEDMATRIX_BUFFER_SIZE LEDMATRIX_WIDTH * 2
+typedef byte buffer[LEDMATRIX_BUFFER_SIZE];
+
+struct PixelIndex {
+    byte byteIndex;
+    byte bitIndex;
+};
+
+class BufferedFrame {
+    public:
+        BufferedFrame();
+        void setPixel(Point p, bool state);
+        bool getPixel(Point p);
+        bool getFrontPixel(Point p);
+        void swapBuffers();
+        void clearBackbuffer();
+        void pointToPixelIndex(Point p);
+        buffer &backbuffer;
+        buffer &frontbuffer;
+    private:
+        byte buffer[LEDMATRIX_BUFFER_SIZE*2];
+        byte bufferInUse;
+};
+
+
+#endif
