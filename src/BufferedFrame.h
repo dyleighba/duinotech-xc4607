b1bc953cb03b240f194a466f99907aabda33d118
Dylan Barber <dylan@dlbarber.dev>
2022-06-23 15:50:12 +0800

    Added buffered frame to LEDMatrix namespace

--- src/BufferedFrame.h
+++ src/BufferedFrame.h
@@ -5,28 +5,30 @@
 #include "Point.h"
 
 #define LEDMATRIX_BUFFER_SIZE LEDMATRIX_WIDTH * 2
-typedef byte buffer[LEDMATRIX_BUFFER_SIZE];
 
-struct PixelIndex {
-    byte byteIndex;
-    byte bitIndex;
+namespace LEDMatrix {
+    typedef byte buffer[LEDMATRIX_BUFFER_SIZE];
+
+    struct PixelIndex {
+        byte byteIndex;
+        byte bitIndex;
+    };
+
+    class BufferedFrame {
+        public:
+            BufferedFrame();
+            void setPixel(Point p, bool state);
+            bool getPixel(Point p);
+            bool getFrontPixel(Point p);
+            void swapBuffers();
+            void clearBackbuffer();
+            void pointToPixelIndex(Point p);
+            buffer &backbuffer;
+            buffer &frontbuffer;
+        private:
+            byte buffer[LEDMATRIX_BUFFER_SIZE*2];
+            byte bufferInUse;
+    };
 };
-
-class BufferedFrame {
-    public:
-        BufferedFrame();
-        void setPixel(Point p, bool state);
-        bool getPixel(Point p);
-        bool getFrontPixel(Point p);
-        void swapBuffers();
-        void clearBackbuffer();
-        void pointToPixelIndex(Point p);
-        buffer &backbuffer;
-        buffer &frontbuffer;
-    private:
-        byte buffer[LEDMATRIX_BUFFER_SIZE*2];
-        byte bufferInUse;
-};
-
 
 #endif
